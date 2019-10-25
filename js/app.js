fb850fd1e73468c6ce393f70971bbcfbde380738
JFrank2019 <jfrank2019@163.com>
2019-10-25 21:45:51 +0800

    finish todomvc-vue

--- js/app.js
+++ js/app.js
@@ -1,6 +1,165 @@
-(function (window) {
-	'use strict';
+(function (Vue) {
 
-	// Your starting point. Enjoy the ride!
+	const STORAGE_KEY = 'items-vuejs'
 
-})(window);
+	const itemStorage = {
+		// 获取本地数据
+		fetch() {
+			return JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]')
+		},
+		// 保存本地数据
+		save(items) {
+			localStorage.setItem(STORAGE_KEY, JSON.stringify(items))
+		}
+	}
+
+	const items = [
+		// {
+		// 	id: 1,
+		// 	content: 'Vue.js',
+		// 	completed: false // 是否完成
+		// },
+		// {
+		// 	id: 2,
+		// 	content: 'React.js',
+		// 	completed: true
+		// },
+		// {
+		// 	id: 3,
+		// 	content: 'Angular.js',
+		// 	completed: true
+		// }
+	]
+
+	// 用于刷新页面时自动聚焦
+	Vue.directive('app-focus', {
+		inserted(el) {
+			el.focus()
+		}
+	})
+
+	const app = new Vue({
+		el: '#todoapp',
+		data() {
+			return {
+				// items,
+				items: itemStorage.fetch(), // 获取本地数据进行初始化
+				currentItem: null, // 当前点击项
+				filterStatus: 'all' // 接收变化的状态值
+			}
+		},
+		computed: {
+			// 过滤所有未完成的任务项
+			remaining() {
+				return this.items.filter(item => !item.completed).length
+			},
+			// 全局开关
+			toggleAll: {
+				get() {
+					// 当 this.remaining发生变化后，会触发该方法运行
+					// 当所有未完成任务数为 0， 表示全部完成，则返回 true，让复选框选中，反之不选中
+					return this.remaining === 0
+				},
+				set(newStatus) {
+					// 当点击checkbox 复选框后状态变化后，就会触发该方法执行
+					// 迭代出数组每个元素，把当前状态值赋给每个元素的 completed
+					this.items.forEach(item => (item.completed = newStatus))
+				}
+			},
+			// 过滤不同的状态数据
+			filterItems() {
+				// this.filterStatus 作为条件，变化后过滤不同数据
+				switch (this.filterStatus) {
+					case 'active': // 过滤未完成的数据
+						return this.items.filter(item => !item.completed)
+						break
+					case 'completed': // 过滤已完成的数据
+						return this.items.filter(item => item.completed)
+						break
+					default: // 其他: 返回所有数据
+						return this.items
+				}
+			}
+		},
+		methods: {
+			addItem(e) {
+				// 1. 获取输入的数据
+				const content = e.target.value.trim()
+				// 2. 如果数据为空，则什么都不做
+				if (!content.length) return
+				// 3. 如果不为空，则添加到数组中 - 生成id、 添加到数组中(默认状态为未完成)
+				const id = this.items.length + 1
+				this.items.push({
+					id,
+					content,
+					completed: false
+				})
+				// 4. 清空文本框内容
+				e.target.value = ''
+			},
+			// 删除项目
+			removeItem(index) {
+				// 移除索引为index的一条记录
+				this.items.splice(index, 1)
+			},
+			// 清除完成的项目
+			removeCompleted() {
+				this.items = this.items.filter(item => !item.completed)
+			},
+			// 编辑项目
+			toEdit(item) {
+				this.currentItem = item
+			},
+			// 取消编辑
+			cancelEdit() {
+				// 移除样式 => 将 currentItem 置空
+				this.currentItem = null
+			},
+			// 编辑完成
+			finishEdit(item, index, event) {
+				const content = event.target.value.trim()
+				// 1. 如果为空，则进行删除任务项
+				if (!content) {
+					this.removeItem(index)
+					return
+				}
+				// 2. 添加任务到任务项 items 中
+				item.content = content
+				// 3. 移除 .editing 样式
+				this.currentItem = null
+			}
+		},
+		watch: {
+			// 如果items发生变速，这个函数就会执行
+			items: {
+				deep: true, // 深度监听
+				handler(newItems, oldItems) {
+					// 本地进行存储
+					itemStorage.save(newItems)
+				}
+			}
+		},
+		// 自定义局部指令，用于编辑输入框
+		directives: {
+			'todo-focus': {
+				// 所在组件的 VNode 更新时调用，但是可能发生在其子 VNode 更新之前。指令的值可能发生了改变，也可能没有。但是你可以通过比较更新前后的值来忽略不必要的模板更新
+				update(el, binding) {
+					if (binding.value) {
+						el.focus()
+					}
+				}
+			}
+		}
+	})
+
+	// 当路由 hash 值改变后会自动调用此函数
+	window.onhashchange = function() {
+		// 1. 获取点击路由的 hash , 当截取的hash不为空返回截取的，为空时返回 'all'
+		const hash = window.location.hash.substr(2) || 'all'
+		// 2. 状态一旦改变，将hash赋值给 filterStatus
+		// 当计算属性 filterItems 感知到 filterStatus 变化后，就会重新过滤
+		// 当 filterItems 重新过滤出目标数据后，则自动同步更新到视图中
+		app.filterStatus = hash
+	}
+
+})(Vue);
--- readme.md
+++ readme.md
@@ -1,19 +1,54 @@
-# TodoMVC App Template
+# TodoMVC - Vue.js
 
-> Template used for creating [TodoMVC](http://todomvc.com) apps
+## 需求说明
 
-![](https://github.com/tastejs/todomvc-app-css/raw/master/screenshot.png)
+1. 数据列表渲染
 
+   - 当任务列表（items ）没有数据时， `#main` 和`#footer` 标识的标签应该被隐藏
 
-## Getting started
+     任务涉及字段 : id 、任务名称（name ）、是否完成（ `completed` true 为已完成）
 
-- Read the [Application Specification](https://github.com/tastejs/todomvc/blob/master/app-spec.md) before touching the template.
+2. 添加任务
 
-- Delete this file and rename `app-readme.md` to `readme.md` and fill it out.
+   - 在最上面的文本框中添加新的任务。
+   2. 不允许添加非空数据。
+   3. 按 `Enter` 键添加任务列表中，并清空文本框。
+   4. 当加载页面后文本框自动获得焦点，在 `input` 上使用 `autofocus` 属性可获得。
 
-- Clone this repo and install the dependencies with [npm](https://npmjs.com) by running: `npm install`.
+3. 显示所有未完成任务数
 
+   - 左下角要显示未完成的任务数量。确保数字是由 `<strong>` 标签包装的。
 
-## License
+   - 还要将 `item` 单词多元化( `1` 没有`s` , 其他数字均有s )： 0 items , 1 item , 2 items
 
-<a rel="license" href="http://creativecommons.org/licenses/by/4.0/deed.en_US"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by/4.0/80x15.png" /></a><br />This <span xmlns:dct="http://purl.org/dc/terms/" href="http://purl.org/dc/dcmitype/InteractiveResource" rel="dct:type">work</span> by <a xmlns:cc="http://creativecommons.org/ns#" href="http://sindresorhus.com" property="cc:attributionName" rel="cc:attributionURL">TasteJS</a> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/4.0/deed.en_US">Creative Commons Attribution 4.0 International License</a>.
+     示例： `2 items left`
+
+4. 切换所有任务状态
+
+   - 点击复选框 `V` 后，将所有任务状态标记为复选框相同的状态。
+   - 当 选中/取消 了单个任务时，复选框 `V` 也应同步更新。
+
+5. 移除任务项
+
+   - 悬停在某个任务项上显示 `X` 移除按钮，可点击移除当前任务项。
+
+6. 清除所有已完成任务
+
+   - 单击右下角 `Clear completed` 按钮时，移除所有已完成任务。
+   - 单击 `Clear completed` 按钮后，确保复选框清除了选中状态。
+   - 当列表中没有已完成的任务时，应该隐藏 `Clear completed` 按钮。
+
+7. 编辑任务项
+
+   - 双击`<label>`（某个任务项）进入编辑状态（在 `<li>` 上通过 .editing 进行切换状态）
+   - 进入编辑状态后输入框显示原内容，并获取编辑焦点
+   - 输入状态按 `Esc` 取消编辑，`editing` 样式应该被移除
+   - 按 `Enter` 键或失去焦点时保存改变数据，移除 `editing` 样式
+
+8. 路由状态切换（过滤不同状态数据）
+
+   根据点击的不同状态（ `All` / `Active` / `Completed` ），进行过滤出对应的任务，并进行样式的切换。
+
+9. 数据持久化
+
+   将所有任务项数据持久化到 `localStorage` 中，它主要式用于本地存储数据
